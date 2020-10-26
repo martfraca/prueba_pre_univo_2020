32bc76a3fbe93cd0491c27f35b765306dda645b6
agreyes <reyes_orellana93@hotmail.com>
2020-10-25 21:21:20 -0600

    Modificado por Antonio G Reyes

--- Integrantes.php
+++ Integrantes.php
@@ -3,4 +3,5 @@
    echo  "Flores Bermudez Marleny Judith - U20090217";
    echo "Benitez Rodriguez Mayra Rebeca -  U20100411";
    echo "Cesar Arturo Zelaya Granados - U20110685";
+   echo "Antonio Guadalupe Reyes Orellana - U20090018";
 ?>
